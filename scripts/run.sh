182f81520165c938f3dec2d3410a86d9b5f0ae71
Emiliano Borghi <eborghiorue@frba.utn.edu.ar>
2020-04-02 18:17:06 -0300

    Clean up run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,7 @@
-docker run --name jupyter-ros -v "$( dirname $( readlink -f $0 ) )/../notebooks:/notebooks" -p 8888:8888 -d jupyter-ros
+xhost +si:localuser:root
+docker run --name jupyter-ros \
+      -e DISPLAY \
+      -v /tmp/.X11-unix:/tmp/.X11-unix \
+      -v "$( dirname $( readlink -f $0 ) )/../notebooks:/notebooks" \
+      -p 8888:8888 \
+      -d jupyter-ros
